2dcd61c961f7ed4bce950194e7be871a1ed7a4f8
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2024-03-22 18:04:48 -0300

    Update matriz_05.js

--- javascript/matrizes/matriz_05.js
+++ javascript/matrizes/matriz_05.js
@@ -1,17 +1,32 @@
-﻿const leia = require("readline-sync");
+const leia = require("readline-sync");
 
 let soma = 0;
 
-let vetorSoma = Array(5)
+/* Vetor que armazenará a soma das colunas*/
+let vetorSoma = Array(4)
+
+/*  Cria a matriz que será utilizada como base para definir o número de linhas
+    Note que o Array está com o tamanho 5, indicando que a Matriz terá 5 linhas
+*/
 let matrizInteiros = new Array(5);
 
+/*  Cria o Laço de Repetição For, que será responsável por criar as 5 linhas com 4 colunas
+    Note que o Array interno está com o tamanho 4, indicando que cada linha da Matriz 
+    terá 4 colunas
+*/
 for (let indiceLinha = 0; indiceLinha < matrizInteiros.length; indiceLinha++){
     matrizInteiros[indiceLinha] = Array(4);
 }
 
 for (let indiceLinha = 0; indiceLinha < matrizInteiros.length; indiceLinha++){
   
-    for (let indiceColuna = 0; indiceColuna < matrizInteiros.length; indiceColuna++){
+    /*  Para obter o tamanho da linha, precisamos indicar a posição da linha
+        dentro da Matriz que estamos lendo. 
+        Neste caso, como todas as linhas terão 4 colunas, eu fixei o valor 0
+        para pegar o tamanho da linha 0. 
+        Poderia também utilizar uma variável pegar o indice de cada linha da matriz
+    */
+    for (let indiceColuna = 0; indiceColuna < matrizInteiros[0].length; indiceColuna++){
       
       matrizInteiros[indiceLinha][indiceColuna] = leia.questionInt(`Digite o numero do elemento [${indiceLinha}][${indiceColuna}]: `);
   
@@ -21,7 +36,8 @@ for (let indiceLinha = 0; indiceLinha < matrizInteiros.length; indiceLinha++){
 
 console.table(matrizInteiros);
 
-for (let indiceColuna = 0; indiceColuna < matrizInteiros.length; indiceColuna++){
+// Observe que os indíces doram invertidos para efetuar a leitura correta das posições da Matriz
+for (let indiceColuna = 0; indiceColuna < matrizInteiros[0].length; indiceColuna++){
   
     for (let indiceLinha = 0; indiceLinha < matrizInteiros.length; indiceLinha++){
       
