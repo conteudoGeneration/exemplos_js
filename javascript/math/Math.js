dd3ada572d889d7b920710c5b75e3a79590fe5c4
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-03-23 18:06:21 -0300

    Update

---
+++ javascript/math/Math.js
@@ -0,0 +1,23 @@
+﻿let i = 27;
+let j = 2.7;
+let x = 10;
+let y = -10;
+let z = 0;
+let w = 9;
+
+console.log(`Constante PI: ${Math.PI}`);
+console.log(`Constante E: ${Math.E}`);
+
+console.log(`\nValor Absoluto de x: ${Math.abs(x)}`);
+console.log(`Valor Absoluto de y: ${Math.abs(y)}`);
+console.log(`Valor Absoluto de z: ${Math.abs(z)}`);
+
+console.log(`\nw elevado ao Quadrado: ${Math.pow(w, 2)}`);
+console.log(`w elevado ao Cubo: ${Math.pow(w, 3)}`);
+
+console.log(`\nRaíz Quadrada de w: ${Math.sqrt(w)}`);
+console.log(`Raíz Cúbica de i: ${Math.cbrt(i)}`);
+
+
+
+
